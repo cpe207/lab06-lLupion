ae782484ac09d5ed4e4c4cb2add33bf948af24f1
Wichuda <Vichudarodna.0569@gmail.com>
2024-07-08 20:41:30 +0700

    done

--- q1.ts
+++ q1.ts
@@ -1,18 +1,34 @@
 // const axios = require("axios");
 import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+}
+
 /* assign interface/type to the function definition properly */
-const getUser = async (userId: number) => {
-  /* Your code here */
+const getUser = async (userId: number): Promise<string> => {
+  try {
+    const response = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user = response.data;
+    return user.name;
+  } catch (error) {
+    return "INVALID USER ID";
+  }
 };
 
 //test case
-const input1 = 1;
-const input2 = 100;
+const run = async () => {
+  const input1 = 1;
+  const input2 = 100;
 
-//run
-getUser(input1).then((result) => console.log(result));
-getUser(input2).then((result) => console.log(result));
+  const result1 = await getUser(input1);
+  const result2 = await getUser(input2);
 
+  console.log(result1);
+  console.log(result2);
+};
+
+run();
 // module.exports = getUser;
 export default getUser;
--- q2.ts
+++ q2.ts
@@ -1,9 +1,36 @@
 // const axios = require("axios");
 import axios from "axios";
 
+interface Todo {
+  userId: number;
+  title: string;
+  completed: boolean;
+}
+
+interface User {
+  id: number;
+  name: string;
+}
+
 /* assign interface/type to the function definition properly */
 const getTodo = async (todoId: number) => {
-  /* Your code here */
+  try {
+    const todoResponse = await axios.get<Todo>(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
+    const todo = todoResponse.data;
+
+    const userResponse = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${todo.userId}`);
+    const user = userResponse.data;
+
+    const result = {
+      owner: user.name,
+      title: todo.title,
+      completed: todo.completed,
+    };
+
+    return result;
+  } catch (error) {
+    return "INVALID TODO ID";
+  }
 };
 
 //test case
@@ -12,8 +39,14 @@ const input2 = 60;
 const input3 = 250;
 
 //run
-getTodo(input1).then((result) => console.log(result));
-getTodo(input2).then((result) => console.log(result));
-getTodo(input3).then((result) => console.log(result));
+Promise.all([
+  getTodo(input1),
+  getTodo(input2),
+  getTodo(input3)
+]).then((results) => {
+  results.forEach((result) => {
+    console.log(result);
+  });
+});
 
 export default getTodo;
